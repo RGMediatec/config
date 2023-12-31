8b6fc460e50e09c2813314c3e6ceac8e8b3d37a4
RGMediatec <73437055+RGMediatec@users.noreply.github.com>
2023-12-31 14:42:21 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -9,15 +9,13 @@ cd ~
 #	install Tool-Set
 ####################
 
-apt update && apt install curl htop bpytop bmon screen tmux nethogs nload iftop nano neofetch ncdu nnn nmon git unzip distro-info net-tools sysbench 
-mc lshw smartmontools ethtool pv sysstat iotop termshark sipcalc
+apt update && apt install curl htop bpytop bmon screen tmux nethogs nload iftop nano neofetch ncdu nmon git unzip distro-info net-tools sysbench lshw smartmontools ethtool pv sysstat iotop sipcalc mc termshark
 
 # configure tmux
 
 mkdir -p ~/.tmux
 # get tmux plugin manager (tpm)
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
 wget -O ~/.tmux.conf https://raw.githubusercontent.com/RGMediatec/config/main/tmux/.tmux.conf
 
 # install sysadmin-utils
@@ -28,18 +26,17 @@ git clone https://github.com/skx/sysadmin-util.git
 # SSH CONFIG
 ####################
 
-mkdir -p ~/.ssh && nano ~/.ssh/authorized_keys
-
+mkdir -p ~/.ssh && touch ~/.ssh/authorized_keys
 sed -i '$assh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIArKDvXiBMJ7QmccKV+p4CdCCsXlz2kXJ5P/XnKmEoa6 philipp@rgmediatec' ~/.ssh/authorized_keys
-
 sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
 sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
 sed -i 's/#PermitRootLogin yes/PermitRootLogin prohibit-password/g' /etc/ssh/sshd_config
 
 systemctl restart sshd.service
 
-#
+####################
 # Aliases
-#
+####################
 
  wget -O ~/.bash_aliases https://raw.githubusercontent.com/RGMediatec/config/main/cfg/.bash_aliases
+ source ~/.bash_aliases
